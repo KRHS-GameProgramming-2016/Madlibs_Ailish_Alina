437130430ec0f3df90c9673c9afa1561d71d71ae
thisisaname <guyhgyhju@hjbjuhbv.com>
2016-10-11 11:28:16 -0400

    I need to see someone. This story is too strange. I added lines of text so that it broke up and would display on the screen properly.

--- therealstory1.py
+++ therealstory1.py
@@ -3,22 +3,23 @@ from input import *
 #this was written by Alina Blake. Deal with it, bruh ;)
 def story():
     adjective1 = getWord("Enter a location: ")
-    temperature1 = getNumber("Enter a Number: ")
+    temperature1 = getNumber("Enter a number: ")
     name1 = getWord ("Enter a name: ")
     verb1 = getWord ("Enter a verb: ")
     noun1 = getWord ("Enter a noun: ")
-    adjective2 = getWord ("Enter a texture") 
+    adjective2 = getWord ("Enter a texture: ") 
+    name2 = getWord ("Enter a name: ")
     
    
    
     text = ""
     text += "One day I randomly decided to time travel to the most pequiliar location."  
     text +=  "This was the home of a strange creature, the" +adjective1 "unicorn."
-    text += " I could not take my eyes off his" +adjective2 +noun1 ", which"
-   
+    text += "I could not take my eyes off his" +adjective2 +noun1 ", which"
+    text += "glistened in the artificial light that was provided by a " +temperature1 
+    text += "flatscreen TV. The figures on the screen were moving, waging a war of some kind."
     text += +name1 "looked at me, his soulless eyes as black as dark matter and obsidian's lovechild. They seemed lethal, until I looked at " +name1 "'s headset and mysteriously levitating Xbox One controller. "
-    text += " "How does a unicorn play Call of Duty?" I asked myself, backing away slowly. It was too late. "
-    text += "
+    text += " \"How does a unicorn play Call of Duty?\" I asked myself, backing away slowly. It was too late. "
     
     
     text += ". It was disconbobulating, like the feeling of a day with the approximate temperature of " + temperature1

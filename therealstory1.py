8b92a5f5d473ad4b63c748185a1caa6b6407bb1b
thisisaname <guyhgyhju@hjbjuhbv.com>
2016-10-11 11:55:02 -0400

    added more word inserts.

--- therealstory1.py
+++ therealstory1.py
@@ -9,21 +9,24 @@ def story():
     noun1 = getWord ("Enter a noun: ")
     adjective2 = getWord ("Enter a texture: ") 
     name2 = getWord ("Enter a name: ")
-    
+    noun2 = getWord ("Enter a time of day: ")
+    verb2 = get word ("Enter a verb: ") 
+    adjective3  = getWord ("Enter a adjective: ")
+    verb3  =  getWord ("Enter a verb ending with ing: ")
    
    
     text = ""
-    text += "One day I randomly decided to time travel to the most pequiliar location."  
-    text +=  "This was the home of a strange creature, the" +adjective1 "unicorn."
+    text += "One" +noun2"," +name2  "randomly decided to" +verb2 "to the most pequiliar location."  
+    text +=  "This was the home of a" +adjective3 "creature, the" +adjective1 "unicorn."
     text += "I could not take my eyes off his" +adjective2 +noun1 ", which"
     text += "glistened in the artificial light that was provided by a " +temperature1 
-    text += "flatscreen TV. The figures on the screen were moving, waging a war of some kind."
-    text += +name1 "looked at me, his soulless eyes as black as dark matter and obsidian's lovechild. They seemed lethal, until I looked at " +name1 "'s headset and mysteriously levitating Xbox One controller. "
-    text += " \"How does a unicorn play Call of Duty?\" I asked myself, backing away slowly. It was too late. "
-    
-    
-    text += ". It was disconbobulating, like the feeling of a day with the approximate temperature of " + temperature1
-    text += " Frah' LibEnburr felt very " + e
-    text += " out."    
+    text += "flatscreen TV. The figures on the screen were" +verb3 ", waging a war of some kind."
+    text += +name1 "looked at me, his soulless eyes as black as dark matter and obsidian's lovechild."
+    text += "They seemed lethal, until"  +name2  "looked at" +name1 "'s headset and mysteriously levitating"
+    text += "Xbox One controller. Dorito dust covered the unicorn's hooves and Mountain Dew bottles were everywhere."
+    text += " \"How does a unicorn play Call of Duty?\"" +name2 "asked themselves, backing away slowly. It was too late."
+    text += +name2 "had seen too much. They backed away from the raging unicorn, hastily returning to their own time."
+    text += "They had time travelled quite enough for one day, they decided. They never wanted to see" +name1 "the"
+    text += "unicorn ever again, online or in person."
     return text
      

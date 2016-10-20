2406964692d35a5dcf782a3dea9f126afe86c6e2
thisisaname <guyhgyhju@hjbjuhbv.com>
2016-10-20 11:24:11 -0400

    started typing a rough story and began to add commands.

--- story3.py
+++ story3.py
@@ -1,14 +1,24 @@
 from input import *
 
-#Written by Mr. Spooner
+#Written by Alina Blake
 def story():
-    location1 = getWord("Enter a location: ")
-    temperature1 = getNumber("Enter a Number: ")
-    
-    text = ""
-    text += "One day I went to the " + location1
-    text += ". It was like a " + temperature1
-    text += " out."    
-    return text
+    size1 = getWord("Enter a word that describes the overall size of a canine figure: ")
+    dogbreed1 = getWord("Enter a dog breed: ")
+    texture1 = getWord("Enter a texture of the ground: ")
 
-story()
+#story()
+
+The *size* *dog breed* trotted along the alleyway, paws hardened after walking along 
+hard, brittle surfaces for too long. As *he/she/they if youre obsessed with
+gender neutral pronouns* reached the end of their little, private path, bright
+lights assaulted their eyes. *gender* always hated emerging onto a bustling sidewalk.
+It was always challenging for them to walk down *generic street name* Avenue, they jumped, 
+bobbed, and wove between legs. It was like *popular TV show* (preferrably not wipeout or 
+american ninja warrior), except that you were little over *super small height that should
+not make sense*. Life was a challenge, to say the least. 
+
+text = ""
+text += "The" +size1
+text += +dogbreed1 "trotted along the alleyway, paws hardened after walking along"
+text += +texture1 "streets for too long. As" 
+text += " *words go here* "

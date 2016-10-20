1528a45a3a09481a7247599e244755284f77a473
riomhchlaru <ailish.olearyaustin@kearsarge.org>
2016-10-20 11:21:31 -0400

    fixed! now all updated w/ choices

--- story2.py
+++ story2.py
@@ -14,7 +14,7 @@ from input import *
     #return text
 #print story
 
-def beginning():
+def story():
     
     firstname1 = getWord("Enter a first name: ")
     surname1 = getWord("Enter a last name: ")
@@ -23,9 +23,9 @@ def beginning():
     weather1 = getWord("Enter an adjective describing weather: ")
     city1 = getWord("Enter the name of a city: ")
     profession1 = getWord("Enter a profession, like businessman or fisherman: ")
-    firstname2 = getWord("Enter another first name: ")
-    surname2 = getWord("Enter another last name: ")
-    fullname2 = firstname2 + " " + surname2
+    victimfirst = getWord("Enter another first name: ")
+    victimsur = getWord("Enter another last name: ")
+    victimfull = victimfirst + " " + victimsur
     adjective1 = getWord("Enter an adjective: ")
     noun1 = getWord("Enter a noun: ")
     adjective2 = getWord("Enter an adjective: ")
@@ -36,9 +36,9 @@ def beginning():
     text += "It was a " +weather1
     text += " day in " +city1
     text += " when our story began, and I was on my way to the scene of the crime. The murder of a wealthy " +profession1
-    text += ", Mr. " +fullname2
+    text += ", Mr. " +victimfull
     text += ", had just been reported by the butler at his giant, " +adjective1
-    text += " mansion. \n \n When I arrived, the body of Mr. " +surname2
+    text += " mansion. \n \n When I arrived, the body of Mr. " +victimsur
     text += " lay in a pool of blood, a " +noun1
     text += " stabbed into his chest. The butler stood by the body, distraught. " 
     text += "\n\"Who would do such a thing?\" the butler cried. A" +adjective2
@@ -46,20 +46,17 @@ def beginning():
     text += "\n\"Yes, who? And why?\" she added, seemingly as upset as the butler."
     text += "\n\"I\'ll get to the bottom of this,\" I reassured them. \"They don't call me" +adjective3 + " " +surname1
     text += " for nothing. "
-    text += "\n Despite their apparent distress at Mr. " +surname2
+    text += "\n Despite their apparent distress at Mr. " +victimsur
     text += "\'s death, I had my suspicions about the butler and the maid."
     text += "\n \n Who should I investigate first? "
     text += "\n\n  A) The butler"
     text += "\n  B) The maid \n"
-    return text
-
-def story():
-    print beginning()
+    print text
     choice = getSuspectOption()
     if choice == "a":
-        print ButlerStory()
+        print ButlerStory(victimfirst, victimsur)
     if choice == "b":
-        print MaidStory()
+        print MaidStory(victimfirst, victimsur)
 
 def getSuspectOption():
     goodInput = False
@@ -73,14 +70,18 @@ def getSuspectOption():
             print "Please make a valid selection!"
     return response.lower()
 
-def MaidStory():
+def MaidStory(victimfirst, victimsur):
+    victimfull = victimfirst + " " + victimsur
+   
     text = "\n"
-    text += "I decided to investigate the maid first. "
+    text += "I decided to investigate the maid first. " +victimfull
     return text
 
-def ButlerStory(): 
+def ButlerStory(victimfirst, victimsur): 
+    victimfull = victimfirst + " " + victimsur
+    
     text = "\n"
-    text += "I decided it wise to investigate the butler first. "
+    text += "I decided it wise to investigate the butler first. " +victimfull
     return text
 
 story()

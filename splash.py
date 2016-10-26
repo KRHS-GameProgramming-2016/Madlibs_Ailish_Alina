f9f2dbb4e6ea4331dac3838051ddd4cd6ab9d7dd
riomhchlaru <ailish.olearyaustin@kearsarge.org>
2016-10-26 11:49:09 -0400

    made the splash screen pretty

--- splash.py
+++ splash.py
@@ -1,16 +1,19 @@
 def splash():
     screen = ""
-    screen += "*********************************\n"
-    screen += "*                               *\n"
-    screen += "*           Welcome             *\n"
-    screen += "*             to                *\n"
-    screen += "*           Madlibs             *\n"
-    screen += "*                               *\n"
-    screen += "*                               *\n"
-    screen += "*       by Alina                *\n"
-    screen += "*                               *\n"
-    screen += "*       press enter to continue *\n"
-    screen += "*********************************\n"
+    screen += "    *****************************\n"
+    screen += "   *******************************\n"
+    screen += "  **                             **\n"
+    screen += " **           Welcome             **\n"
+    screen += "***             to                ***\n"
+    screen += "***           Madlibs             ***\n"
+    screen += "***                               ***\n"
+    screen += "***                               ***\n"
+    screen += "***      by Alina and Ailish      ***\n"
+    screen += "***                               ***\n"
+    screen += " **    press enter to continue    **\n"
+    screen += "  **                             **\n"
+    screen += "   *******************************\n"
+    screen += "    *****************************\n"
     return screen
 
 
@@ -21,10 +24,12 @@ def menu():
     screen += "*                               *\n"
     screen += "*   1) Story One                *\n"
     screen += "*   2) Story Two                *\n"
-    screen += "*   3) Story Three                *\n"
+    screen += "*   3) Story Three              *\n"
     screen += "*                               *\n"
     screen += "*   Q) Quit                     *\n"
     screen += "*                               *\n"
     screen += "*                               *\n"
     screen += "*********************************\n"
     return screen
+
+print splash()
